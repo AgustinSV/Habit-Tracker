3c488a15e6cde56e44a27e8f3340b9c2a53dedd6
AgustinSV <agustin1salasvaca@gmail.com>
2025-06-02 22:50:21 -0400

    initial commit

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    interface CardProps {
+        category: string;
+    }
+
+    const { category = 'Default Category' }: CardProps =  $props();
+</script>
+
+<div class="card">
+    <p class="category">{category}</p>
+</div>
+
+<style>
+    .card {
+        border: 1px solid red;
+        color: white;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import Card from '$lib/components/Card.svelte';
+    import '../app.css';
+    
+    interface SavedItem {
+        id: number;
+        category: string;
+    }
+
+    const savedItems: SavedItem[] = [
+        {
+            id: 1,
+            category: 'Intellect'
+        },
+        {
+            id: 2,
+            category: 'Physical'
+        }
+    ];
+</script>
+
+<div class="card-container">
+    {#each savedItems as item (item.id)}
+        <Card category={item.category} /> {/each}
+</div>
+
+<style>
+    .card-container {
+        border: 1px solid yellow;
+    }
+</style>
